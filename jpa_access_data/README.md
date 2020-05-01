5c8a5aa98850e4eebc3d5c94dbda9ada25d88577
NotSteveWozniak <randle.boyd@gmail.com>
2020-04-30 21:54:49 -0500

    initial commit

---
+++ jpa_access_data/README.md
@@ -0,0 +1,7 @@
+** Read ME **
+
+mvn clean package
+
+java -jar target\jpa_access_data-0.0.1-SN
+APSHOT.jar
+
---
+++ jpa_access_data/src/test/java/sb_tutorial/jpa_access_data/JpaAccessDataApplicationTests.java
@@ -0,0 +1,13 @@
+package sb_tutorial.jpa_access_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAccessDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
